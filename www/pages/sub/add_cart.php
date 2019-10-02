52859250a2f191748d026a82cd692093ec5ff885
Raey22 <japiraey.h.rh@gmail.com>
2019-10-02 08:55:20 -0500

    fixed the first time add_cart bug

--- www/pages/sub/add_cart.php
+++ www/pages/sub/add_cart.php
@@ -2,7 +2,14 @@
 	session_start();
 
 	//check if product is already in the cart
-	if(!in_array($_GET['id'], $_SESSION['cart_array'])){
+	if(is_null($_SESSION['cart_array'])){
+		array_push($_SESSION['cart_array'], $_GET['id']);
+		array_push($_SESSION['co_quantity'], 1);
+		$_SESSION['co_price'] = number_format((float)((($_GET['w']*$_GET['h']) * $_GET['p'])* $_SESSION['co_quantity'][sizeof($_SESSION['co_quantity'])-1]+$_SESSION['co_price']), 2, '.', '');
+
+		$_SESSION['success_msg'] = 'Product added to cart';
+	}
+	else if(!in_array($_GET['id'], $_SESSION['cart_array'])){
 		array_push($_SESSION['cart_array'], $_GET['id']);
     array_push($_SESSION['co_quantity'], 1);
 
